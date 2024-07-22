e0aef5149f42c7dab19721a3c0f4997da52d0ac4
70928manson <manson972@yahoo.com>
2024-07-22 09:48:58 +0800

    updae: user model username, email 加上 required 提示訊息

--- models/User.ts
+++ models/User.ts
@@ -7,20 +7,21 @@ const userSchema = new Schema(
         username: {
             type: String,
             unique: true,
-            required: true,
+            required: [true, "username must be filled."],
         },
         email: {
             type: String,
             unique: true,
-            required: true,
+            required: [true, "email must be filled."],
         },
         password: {
             type: String,
             required: false,
-            select: false,
+            // select: false,
         },
     },
     { timestamps: true }
 );
 
+// mongoose.model("User", userSchema) => 需注意這邊的第一個參數User, 在Mongoose中會自動視為小寫開頭及改為複數（結尾加 s), 因此 User 實際上是連到 users collection
 export default mongoose.models.User || mongoose.model("User", userSchema);
