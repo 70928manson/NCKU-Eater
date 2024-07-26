1327cc96691349379504a986426608a75b573d74
70928manson <manson972@yahoo.com>
2024-07-26 13:51:15 +0800

    update: 更新 Navbar compnent rwd

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import Logo from './Logo';
 const Navbar = () => {
   return (
       <header className="w-full fixed bg-white z-50 p-4">
-          <div className="w-[80%] mx-auto flex justify-between">
+          <div className="w-[95%] md:w-[85%] mx-auto flex justify-between">
               <Logo />
               <LoginButton />
           </div>
