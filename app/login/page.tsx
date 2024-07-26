61ce83bf3a810f19e43f3adb159f59218e0e2245
70928manson <manson972@yahoo.com>
2024-07-26 13:54:37 +0800

    style: 更新 login page 樣式

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,11 +3,12 @@ import AuthForm from "./components/AuthForm";
 export default function Login() {
 
     return (
-        <div className="flex min-h-screen flex-col items-center w-full">
+        <div className="h-[calc(100vh-65px)] w-full">
             <div
-                className="flex min-h-screen flex-col justify-center py-12
-            sm:px-6 lg:px-8 bg-gray-100 w-full
-          "
+                className="
+                  flex flex-col justify-center py-12
+                  px-5 sm:px-6 lg:px-8 bg-gray-100 w-full h-full
+                "
             >
                 <AuthForm />
             </div>
