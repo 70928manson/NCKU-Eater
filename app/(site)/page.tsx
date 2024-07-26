dfa9b799fc2a8632cd40d5e9b6753ea5405671f2
70928manson <manson972@yahoo.com>
2024-07-26 14:16:18 +0800

    style: 更新 site page syle

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,9 +2,9 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center pt-16">
-      <div className="">
-        <h2 className="text-3xl tracking-widest">NCKU Food Draw</h2>
+    <main className="flex flex-col items-center">
+      <div className="pt-16">
+        <h2 className="text-2xl md:text-3xl tracking-widest">NCKU Food Draw</h2>
         <section>
           {/* 
             draw titlle
