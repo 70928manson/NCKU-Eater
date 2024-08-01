0c25179c5204ad317d1fdcfc47cc8ce2d24aa473
70928manson <manson972@yahoo.com>
2024-08-01 11:06:51 +0800

    modify: route group site 改變名稱為 root

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="text-2xl md:text-3xl head-text tracking-widest">NCKU Food Draw</h2>
+      <div>
+        {/* draw titlle
+        draw button
+        switch option */}
+        test
+      </div>
+    </div>
+  );
+}
