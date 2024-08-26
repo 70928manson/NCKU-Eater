662a28d9b508e9a67a33a4bc2c4345276ae25f7c
70928manson <manson972@yahoo.com>
2024-08-26 14:51:38 +0800

    modify: 更新 root page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,16 @@
-
+import FoodLottery from "./components/FoodLottery";
+import LocationMap from "./components/LocationMap";
 
 export default function Home() {
+
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="text-2xl md:text-3xl head-text tracking-widest">NCKU Food Draw</h2>
-      <div>
-        {/* draw titlle
-        draw button
-        switch option */}
-        test
+    <div className="flex flex-col items-center w-full">
+      <h2 className="text-2xl md:text-3xl head-text tracking-widest mb-6">
+        NCKU Food Draw
+      </h2>
+      <div className="text-light-1 w-full flex flex-col items-center">
+        <FoodLottery />
+        <LocationMap />
       </div>
     </div>
   );
