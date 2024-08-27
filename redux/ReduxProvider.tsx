ecde8775a5c995a39ed8adc3ff6f490d170ce596
70928manson <manson972@yahoo.com>
2024-08-27 14:57:48 +0800

    modify: 更新 redux provider name

--- redux/ReduxProvider.tsx
+++ redux/ReduxProvider.tsx
@@ -3,7 +3,7 @@ import { useRef } from 'react';
 import { Provider } from 'react-redux';
 import { makeStore, AppStore } from '../redux/store';
 
-export default function StoreProvider({
+export default function ReduxProvider({
     children,
 }: {
     children: React.ReactNode
