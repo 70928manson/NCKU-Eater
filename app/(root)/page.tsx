31f845cd6885415d09604f116dff6ae384d7efe9
70928manson <manson972@yahoo.com>
2024-08-27 15:06:11 +0800

    feat: 首頁取得店家資料

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import FoodLottery from "./components/FoodLottery";
 import LocationMap from "./components/LocationMap";
 
-export default function Home() {
+import { useGetAllStoresQuery } from "@/redux/services/storeServices";
 
+export default function Home() {
+  //抓出全部的店家資料
+  const { data, error, isLoading } = useGetAllStoresQuery("all");
+
+  console.log("data", data);
+  
   return (
     <div className="flex flex-col items-center w-full">
       <h2 className="text-2xl md:text-3xl head-text tracking-widest mb-6">
