3566f06953607442614518d3fb305461b7fd663d
70928manson <manson972@yahoo.com>
2024-08-27 15:08:08 +0800

    feat: Navbar component 新增 account dropdown - 功能列表 Profile, Sign Out

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,16 +2,27 @@
 
 import Link from 'next/link';
 
-import { Bird, LogOutIcon } from 'lucide-react';
+import { Bird, LogOutIcon, UserIcon } from 'lucide-react';
 import { useSession } from 'next-auth/react';
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import {
+    LogOut,
+} from "lucide-react"
 
 const Navbar = () => {
     const session = useSession();
     const isUserLogIn = session?.status === "authenticated";
 
-    console.log("session", session)
 
     return (
         <header>
@@ -23,12 +34,26 @@ const Navbar = () => {
                 {
                     isUserLogIn ? (
                         <div className="flex cursor-pointer gap-2 items-center">
-                            <LogOutIcon className="md:hidden" color="white" size={24} />
-                            <p className="text-light-1">你好!~ {session?.data?.user?.username}</p>
-                            <Avatar>
-                                <AvatarImage src="https://github.com/shadcn.png" />
-                                <AvatarFallback>CN</AvatarFallback>
-                            </Avatar>
+                            {/* <LogOutIcon className="md:hidden" color="white" size={24} /> */}
+                            {/* <p className="text-light-1"></p> */}
+                            <DropdownMenu>
+                                <DropdownMenuTrigger>
+                                    <Avatar>
+                                        <AvatarImage src="https://github.com/shadcn.png" />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent>
+                                    <DropdownMenuLabel>{session?.data?.user?.username ? session?.data?.user?.username : 'Account'}</DropdownMenuLabel>
+                                    <DropdownMenuSeparator />
+                                    <DropdownMenuItem> 
+                                        <UserIcon className="mr-2 h-4 w-4" /> Profile
+                                    </DropdownMenuItem>
+                                    <DropdownMenuItem>  
+                                        <LogOutIcon className="mr-2 h-4 w-4" />Sign Out
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
                         </div>
                     ) : (
                             <div className="font-light flex gap-2 text-dark-1 text-sm tracking-widest border bg-white rounded px-4 py-1 cursor-pointer hover:bg-light-2">
