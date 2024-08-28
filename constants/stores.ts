730f46007d4e9bf46ee06fd10280b228406465c5
70928manson <manson972@yahoo.com>
2024-08-28 14:34:35 +0800

    modify: 更新 mock store data

--- constants/stores.ts
+++ constants/stores.ts
@@ -1,6 +1,26 @@
-export const store = [
+export const stores = [
     {
-        a: '1',
-        b: '2'
-    }
+        _key: 1,
+        name: "小妞炒飯"
+    },
+    {
+        _key: 2,
+        name: "赤嵌樓居酒屋"
+    },
+    {
+        _key: 3,
+        name: "吃飯的小站"
+    },
+    {
+        _key: 4,
+        name: "紅樓小館"
+    },
+    {
+        _key: 5,
+        name: "肉肉控"
+    },
+    {
+        _key: 6,
+        name: "穆斯林的店"
+    },
 ]
