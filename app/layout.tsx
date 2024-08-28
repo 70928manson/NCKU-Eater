89a21da9ef9736d12c78041bd022938baa224160
70928manson <manson972@yahoo.com>
2024-08-28 14:15:56 +0800

    modify: layout 移動位置至 app 資料夾底下

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
 import { Inter, Mochiy_Pop_One } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
+
 import ToasterContext from "./context/ToasterContext";
 import AuthContext from "./context/AuthContext";
+import ReduxProvider from "@/redux/ReduxProvider";
+
+import Navbar from "@/components/shared/navbar/Navbar";
+import Sidebar from "@/components/shared/sidebar/Sidebar"
+import Footer from "@/components/shared/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 const mochiyPopOne = Mochiy_Pop_One({ weight: ["400"], subsets: ["latin"] });
@@ -23,9 +28,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${mochiyPopOne.className} ${inter.className}`}>
         <AuthContext>
-          <ToasterContext />
-          <Navbar />
-          {children}
+          <ReduxProvider>
+            <ToasterContext />
+            <Navbar />
+            <main className="flex flex-row">
+              <Sidebar />
+              <section className="main-container">
+                <div className="w-full max-w-4xl mx-auto">
+                  {children}
+                </div>
+              </section>
+            </main>
+            <Footer />
+          </ReduxProvider>
         </AuthContext>
       </body>
     </html>
