86e6df5e0ca67d3c9be36746eb38298d362530cd
70928manson <manson972@yahoo.com>
2024-09-04 14:13:36 +0800

    modify: 更新 favoriteStores schema 變數名稱

--- models/User.ts
+++ models/User.ts
@@ -30,7 +30,7 @@ const userSchema = new Schema(
             type: String,
             required: false,
         },
-        favoriteStore: {
+        favoriteStores: {
             type: [storeSchema],
             required: false,
         },
