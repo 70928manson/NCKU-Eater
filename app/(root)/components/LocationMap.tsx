66e7426ea40671c6ac52ed462c20ed3ba3064673
70928manson <manson972@yahoo.com>
2024-09-04 15:28:08 +0800

    feat: 新增加入店家至我的最愛功能 - 簡記: icon, onClick, get 最新 session, checkIfStoreIsFavorite, tooltip

--- app/(root)/components/LocationMap.tsx
+++ app/(root)/components/LocationMap.tsx
@@ -3,38 +3,121 @@ import { RootState } from '@/redux/store';
 import { useSelector } from 'react-redux';
 import clsx from 'clsx';
 import { Store } from '@/app/types/store';
+import { HeartIcon } from 'lucide-react';
+import { getSession, useSession } from 'next-auth/react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 const LocationMap = () => {
     const [mapSrc, setMapSrc] = useState("");
+    const [isFavorite, setIsFavorite] = useState(false);
+
     const store = useSelector((state: RootState) => state.lottery.store);
+    const selectedTags = useSelector((state: RootState) => state.lottery.selectedTags);
+
     const mapRef = useRef<HTMLIFrameElement>(null);
 
+    const { data: session } = useSession();
+
     const getGoogleMapContent = (store: Store) => {
+        console.log("google store", store)
         const src = store.src;
         const duration = 1500; // 拉霸效果執行多久
         setTimeout(() => {
             setMapSrc(src);
+            checkIfStoreIsFavorite();
         }, duration);
     };
+
+    const handleFavorite = () => {
+        console.log("user data", session?.user);
+        if (session?.user?.email && store?.title.length > 0) {
+            if (isFavorite) {
+                // 移除
+            } else {
+                const data = {
+                    email: session?.user?.email,
+                    store: store
+                };
+                axios.post('api/addFavorite', data)
+                    .then((res) => {
+                        console.log("res", res)
+                        checkIfStoreIsFavorite()
+                    })
+                    .catch((err) => {
+                        console.log("err", err)
+                        // toast.error("Someting went wrong!");
+                        toast.error(err.response.data);
+                    })
+            }
+        }
+    };
+
+    const fetchUpdatedSession = async () => {
+        const session = await getSession();
+        
+        return session;
+    };
+
+    const checkIfStoreIsFavorite = async () => {
+        // 這邊需要最新的session資料比對
+
+        const session = await fetchUpdatedSession();
+
+        if (session?.user?.favoriteStores) {
+            const isAlreadyFavorite = session.user.favoriteStores.find(
+                (favoriteStore) => favoriteStore.title === store.title
+            );
+
+            if (isAlreadyFavorite) {
+                setIsFavorite(true);
+            } else {
+                setIsFavorite(false)
+            }
+        }
+    }
 
     useEffect(() => {
         getGoogleMapContent(store);
     }, [store]);
 
+    useEffect(() => {
+        setIsFavorite(false);
+    }, [selectedTags])
+
     return (
-        <div className="w-full h-[25vh] xs:h-[40vh] md:h-[50vh] bg-white text-dark-1 relative">
-            <div className={clsx("w-full h-full absolute bg-dark-4", mapSrc && 'hidden')}>
-                <div className="w-full h-full flex justify-center items-center text-gray-1">Google map 顯示區</div>
+        <div className="flex w-full h-full items-start">
+            <div className="w-full h-[25vh] xs:h-[40vh] md:h-[50vh] bg-white text-dark-1 relative mr-1">
+                <div className={clsx("w-full h-full absolute bg-dark-4", mapSrc && 'hidden')}>
+                    <div className="w-full h-full flex justify-center items-center text-gray-1">Google map 顯示區</div>
+                </div>
+                <iframe
+                    title="googleMap"
+                    ref={mapRef}
+                    loading="lazy"
+                    className="transition-all duration-500 ease-in-out w-full h-full"
+                    src={mapSrc}
+                    allowFullScreen
+                    referrerPolicy="no-referrer-when-downgrade">
+                </iframe>
             </div>
-            <iframe
-                title="googleMap"
-                ref={mapRef}
-                loading="lazy"
-                className={clsx("transition-all duration-500 ease-in-out w-full h-full")}
-                src={mapSrc}
-                allowFullScreen
-                referrerPolicy="no-referrer-when-downgrade">
-            </iframe>
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <HeartIcon onClick={handleFavorite} color={isFavorite ? 'red' : 'white'} size={24} />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                        {isFavorite ? <p>移除我的最愛</p> : <p>加入我的最愛</p>}
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
         </div>
     )
 }
