c6591dc9a6238a41cd600874f67e3e663804bd86
70928manson <manson972@yahoo.com>
2024-09-10 15:29:03 +0800

    feat: favorite page 拆分 tagList component

---
+++ components/tagList/tagList.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+interface ModalProps {
+    tags: string[];
+    maxTagLength: number
+}
+
+const TagList: React.FC<ModalProps> = ({ tags, maxTagLength }) => (
+    <div className="flex flex-wrap gap-2">
+        {tags.length > 0 && tags[0] !== "" && tags.map((tag, index) => (
+            <span
+                key={index}
+                className="bg-gray-300 text-gray-700 px-2 py-1 rounded-full text-sm inline-flex justify-center"
+                style={{ minWidth: `${maxTagLength}ch` }}
+            >
+                {tag}
+            </span>
+        ))}
+    </div>
+);
+
+export default TagList
