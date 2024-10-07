875d5159587f2d2da75fb61e16632a2f7f09cc43
70928manson <manson972@yahoo.com>
2024-10-07 09:39:41 +0800

    modify: add delete success notify and default tag to keep height

--- components/tagList/tagList.tsx
+++ components/tagList/tagList.tsx
@@ -7,7 +7,7 @@ interface ModalProps {
 
 const TagList: React.FC<ModalProps> = ({ tags, maxTagLength }) => (
     <div className="flex flex-wrap gap-2">
-        {tags.length > 0 && tags[0] !== "" && tags.map((tag, index) => (
+        {tags.length > 0 && tags[0] !== "" ? tags.map((tag, index) => (
             <span
                 key={index}
                 className="bg-gray-300 text-gray-700 px-2 py-1 rounded-full text-sm inline-flex justify-center"
@@ -15,7 +15,15 @@ const TagList: React.FC<ModalProps> = ({ tags, maxTagLength }) => (
             >
                 {tag}
             </span>
-        ))}
+        )) : (
+            <span
+                key="default"
+                className="bg-gray-300 text-gray-700 px-2 py-1 rounded-full text-sm inline-flex justify-center invisible"
+                style={{ minWidth: `${maxTagLength}ch` }}
+            >
+                default
+            </span>
+        )}
     </div>
 );
 
