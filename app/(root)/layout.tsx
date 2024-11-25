e1c662c10524ff49bfade8f8318bc44e07d936f0
70928manson <manson972@yahoo.com>
2024-11-25 09:39:13 +0800

    modify: update index and auth page layout name

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,15 +6,12 @@ import Sidebar from "@/components/shared/sidebar/Sidebar"
 import Footer from "@/components/shared/footer/Footer";
 
 export const metadata: Metadata = {
-  title: "NCKU Eater 2.0 | 成大美食抽抽樂",
-  description: "探索台南成功大學附近的必吃美食，讓美食抽抽樂 NCKU Eater 替你解決不知道要吃什麼的煩惱",
-  keywords: ['成大', '成功大學', 'NCKU', '台南', '成大美食推薦', '成大附近好吃的', '不知道吃什麼'],
-  verification: {
-    google: '-BSThK2Ki5W6D4AL-CW8xgyuN3jiTEprX2LfeeN7A48',
-  },
+  title: {
+    absolute: "NCKU Eater 2.0 | 成大美食抽抽樂"
+  }
 };
 
-export default function RootLayout({
+export default function DrawLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
